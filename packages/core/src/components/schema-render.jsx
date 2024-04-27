2b56d14f3785c134c49aa44fee0f58de111de02f
Zachary Hueras <zachary.hueras@gmail.com>
2024-04-27 17:44:25 -0400

    feature(core): add form subrendering
    Allows us to use the model context from the form generator, which lets
    us take advantage of zustand's shallow selectors.

---
+++ packages/core/src/components/schema-render.jsx
@@ -0,0 +1,41 @@
+import React, { useMemo } from 'react';
+import { useSchema } from '@forml/hooks';
+
+import { merge } from '../forms';
+import { SchemaField } from './schema-field';
+
+function useGenerator(generator) {
+    if (typeof generator === 'function') {
+        // The generator is a hook; use it
+        return generator();
+    } else {
+        return generator;
+    }
+}
+
+export function SchemaRender(props) {
+    const schema = useSchema();
+    const form = useGenerator(props.form);
+    const merged = useMemo(function() {
+        return merge(schema, form);
+    }, [schema, form]);
+
+    const children = useMemo(
+        () =>
+            merged.map((form, index) => {
+                if (!form) return;
+                const { schema } = form;
+                return (
+                    <SchemaField
+                        key={index}
+                        schema={schema}
+                        form={form}
+                        onChange={props.onChange}
+                    />
+                );
+            }),
+        [merged, props.onChange]
+    );
+    return <>{children}</>
+}
+
