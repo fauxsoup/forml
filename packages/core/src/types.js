8fca4d2a87b4e7ec51a92b47d4a4af2c5811a082
Zachary Hueras <zachary.hueras@gmail.com>
2024-04-27 17:51:56 -0400

    fix(core): custom validator for FormTypes
    Allows mixed types, which the prop-types approach was not happy about

--- packages/core/src/types.js
+++ packages/core/src/types.js
@@ -1,8 +1,23 @@
 import t from 'prop-types';
 
-export const FormType = t.shape({
-    key: t.arrayOf(t.oneOfType([t.string, t.number])),
-    type: t.string,
-});
-export const FormsType = t.arrayOf(t.oneOfType([t.string, FormType]));
+export const FormType = t.oneOfType([
+    t.string,
+    t.shape({
+        key: t.arrayOf(t.oneOfType([t.string, t.number])),
+        type: t.string,
+    }),
+]);
+export const FormsType = t.arrayOf(
+    function (propValue, key, componentName, location, propFullName) {
+        const isString = typeof propValue[key] === 'string';
+        const isObject =
+            typeof propValue[key] === 'object' && propValue[key] !== null;
+
+        if (!isString && !isObject) {
+            return new Error(
+                `Invalid prop \`${propFullName}\` of type \`${typeof propValue[key]}\` supplied to \`${componentName}\`, expected \`string\` or \`object\`.`
+            );
+        }
+    }
+);
 FormType.items = FormsType;
